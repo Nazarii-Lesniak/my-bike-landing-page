ceb291945b0a7052d171a55d41bed6f5209f6a09
Nazarii-Lesniak <naziksay@gmail.com>
2026-05-27 22:39:07 +0300

    feat: add basic form submit handler

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  window.alert('Thank you! The form has been successfully submitted!');
+
+  form.reset();
+});
